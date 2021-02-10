df79e44578ea66a210d143f4fd8d70776b2a65f1
Alexander Todorov <aleksandar9999a@gmail.com>
2021-02-10 07:03:08 +0200

    Added members table

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,4 +1,5 @@
 import ExF, { Component, CustomElement } from 'exf-ts';
+import { IUser } from '../interfaces/interfaces';
 
 
 @CustomElement({
@@ -6,10 +7,49 @@ import ExF, { Component, CustomElement } from 'exf-ts';
   dependencyInjection: true
 })
 export class Team extends Component {
+  rows: IUser[] = [
+    {
+      id: 1,
+      name: 'Alexander'
+    }
+  ];
+
 	render() {
 		return (
-      <div>
-        team
+      <div className="statistic-table">
+        <h2>Members</h2>
+
+        <table>
+          <thead>
+            <tr>
+              <th>
+                ID
+              </th>
+
+              <th>
+                Name
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {
+              this.rows.map(({ id, name }) => {
+                return (
+                  <tr id={id}>
+                    <td>
+                      {id}
+                    </td>
+
+                    <td className="statistic-table__extended-cell">
+                      {name}
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
       </div>
 		)
 	}
